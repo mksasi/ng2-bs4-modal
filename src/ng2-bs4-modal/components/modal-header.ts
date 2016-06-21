b8c02db98fba10a69d463ce8ebd29b758a3df1a3
Jonathan Vollmer <vollmerj@amazon.com>
2016-06-21 12:54:53 -0700

    allow modal header styling

--- src/ng2-bs4-modal/components/modal-header.ts
+++ src/ng2-bs4-modal/components/modal-header.ts
@@ -4,7 +4,7 @@ import { ModalComponent } from './modal';
 @Component({
     selector: 'modal-header',
     template: `
-        <div class="modal-header">
+        <div class="modal-header" [ngClass]="headerClasses">
             <button *ngIf="showClose" type="button" class="close" data-dismiss="modal" aria-label="Close" (click)="modal.dismiss()">
                 <span aria-hidden="true">&times;</span>
             </button>
@@ -14,5 +14,6 @@ import { ModalComponent } from './modal';
 })
 export class ModalHeaderComponent {
     @Input('show-close') showClose: boolean = false;
+    @Input('header-classes') headerClasses: {[key: string]: string} = {};
     constructor(private modal: ModalComponent) { }
 }
